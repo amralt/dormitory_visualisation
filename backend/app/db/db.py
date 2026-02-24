4b1823710fa0d4b21cc80ea8de93c3a0f22cd9ee
amralt <amralt88@gmail.com>
2026-02-24 13:38:50 +0700

    added files for bd

---
+++ backend/app/db/db.py
@@ -0,0 +1,8 @@
+'''
+Тут запускается бд
+
+вообще все, что не относится к crud, model
+'''
+
+# engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+# def init_db(session: Session) -> None:
