08113afb7911a884fcb39dc924994fa3812dbec5
Mahich-0O <mashaelkova6@gmail.com>
2026-03-22 20:36:27 +0700

    Update crud.py

--- backend/app/db/crud.py
+++ backend/app/db/crud.py
@@ -1,9 +1,35 @@
-'''
-Тут будут храниться функции для работы с базой данных:
-получение студентов по этажу, студентов по комнате, по имени.
+from models import ResidentsBase, Rooms, engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select, or_, cast, String
+
+Session = sessionmaker(engine)
 
 
-Можно сделать фильтрацию во фронтенде или здесь?
+def get_by_room_num(room: int, session) -> list[ResidentsBase]:
+    stat = select(ResidentsBase).where(ResidentsBase.room == room)
+    db_obj = session.scalars(stat).all()
+    return db_obj
 
 
-'''
+def get_by_namepart(s: str, session) -> list[ResidentsBase]:
+    stat = select(ResidentsBase).where(ResidentsBase.kontragent.contains(s))
+    db_obj = session.scalars(stat).all()
+    return db_obj
+
+
+def get_by_number_floorordorm(num: str, session) -> list[ResidentsBase]:
+    stat = select(ResidentsBase).where(
+        or_(
+            cast(ResidentsBase.floor, String) == num,
+            ResidentsBase.dormitory.like(f"%{num}%"),
+        )
+    )
+    db_obj = session.scalars(stat).all()
+    return db_obj
+
+
+with Session() as session:
+    print(
+        *get_by_number_floorordorm("5", session)
+    )  # вывод сейчас выглядит очень плохо, но оно работает,
+    # менять буду в зависимости от того, какую инфу нужно выводить дополнительно
