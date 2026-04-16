f261aa1fc648e5c41f37401beac21b1cecdd1175
amralt <amiralt@gmail.com>
2026-04-16 09:46:48 +0700

    added roles

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,3 +3,26 @@ SEARCH_BY_NAME = "SEARCH_BY_NAME"
 SEARCH_BY_ROOM = "SEARCH_BY_ROOM"
 SEARCH_BY_ID = "SEARCH_BY_ID"
 SEARCH_BY_ROOM_AND_ID = "SEARCH_BY_ROOM_AND_ID"
+
+
+USERS = [
+    {
+        "username": "Ольга Евгеньевна Яковлева",
+        "password": "12345",
+        "role": "admin",
+        "department": None,
+    },
+    {
+        "username": "Анастасия Валерьевна Карпенко",
+        "password": "54321",
+        "role": "decan",
+        "department": "ММФ"
+    },
+    {
+        "username": "Алексей Евгеньевич Захаров",
+        "password": "12345",
+        "role": "decan",
+        "department": "ММФ"
+    },
+
+]
